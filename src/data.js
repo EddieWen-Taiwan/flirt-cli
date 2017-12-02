887233c739c89fb99378dfa0862123ad6ef2191c
Eddie Wen <eddiewen.tw@gmail.com>
2017-12-02 22:00:25 +0800

    chore: add conversations

--- src/data.js
+++ src/data.js
@@ -9,11 +9,46 @@ module.exports = [
 		{ text: '那你是怎麼點燃我的心的？', self: true },
 	],
 	[
-        { text: '我最近想要開始吃素', self: true },
+		{ text: '我最近想要開始吃素', self: true },
 		{ text: '因為你是我的菜', self: true },
-    ],
+	],
 	[
-        { text: '我覺得你蠻可怕的', self: true },
+		{ text: '我覺得你蠻可怕的', self: true },
 		{ text: '因為我怕老婆', self: true },
-    ],
+	],
+	[
+		{ text: '你腦中都是裝大便嗎？', self: false },
+		{ text: '我不許你這樣說自己', self: true },
+	],
+	[
+		{ text: '你跑不跑得快嗎？', self: true },
+		{ text: '不快', self: false },
+		{ text: '那我應該追得到你', self: true },
+	],
+	[
+		{ text: '英文字母 A ~ Z 選兩個', self: true },
+		{ text: '....', self: false },
+		{ text: '我會選 U & I', self: true },
+	],
+	[
+		{ text: '我現在要趕去機場', self: true },
+		{ text: '因為你，我的心正在起飛', self: true },
+	],
+	[
+		{ text: '如果你喜歡一個人的話，你會怎麼做？', self: true },
+		{ text: '....', self: false },
+		{ text: '如果是我的話，我會一直看著他（盯著他', self: true },
+	],
+	[
+		{ text: '你知道明年的今天是什麼日子嗎？', self: true },
+		{ text: '我們在一起滿一週年的日子', self: true },
+	],
+	[
+		{ text: '你幫我做一件事好不好？', self: true },
+		{ text: '做我的女人', self: true },
+	],
+	[
+		{ text: '你知道為什麼會有溫室效應嗎？', self: true },
+		{ text: '因為你讓這個世界慾火焚身', self: true },
+	],
 ];
