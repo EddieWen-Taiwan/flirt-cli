9a9b1e16bba836749c41fcd6d37a7567ed1abc4b
Eddie Wen <eddiewen.tw@gmail.com>
2017-12-03 16:09:50 +0800

    feat: support `--repo` & `--report` options
    Get repository link with `--repo`. Get issue page link and my email
    address with `--report`.

--- src/runOption.js
+++ src/runOption.js
@@ -3,5 +3,19 @@ const packageJson = require('../package.json');
 module.exports = (optionArray) => {
 	if (['--version', '-v'].includes(optionArray[0])) {
 		process.stdout.write(`Current version: ${packageJson.version}\n`);
+
+		return;
+	}
+
+	if (optionArray[0] === '--repo') {
+		process.stdout.write(`Visit <${packageJson.homepage}>\n`);
+
+		return;
+	}
+
+	if (optionArray[0] === '--report') {
+		process.stdout.write(
+			`Open an issue at <${packageJson.bugs.url}>,\nor mail to <${packageJson.bugs.email}>\n`,
+		);
 	}
 };
