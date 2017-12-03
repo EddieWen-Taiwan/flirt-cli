84800a327b2b4e6057e815d4f1816363e51758e2
Eddie Wen <eddiewen.tw@gmail.com>
2017-12-03 16:21:51 +0800

    refactor: add an object to list the supported options

--- src/runOption.js
+++ src/runOption.js
@@ -1,19 +1,34 @@
 const packageJson = require('../package.json');
 
+const support = {
+	version: {
+		commands: ['--version', '-v'],
+		note: '',
+	},
+	repo: {
+		commands: '--repo',
+		note: '',
+	},
+	report: {
+		commands: '--report',
+		note: '',
+	},
+};
+
 module.exports = (optionArray) => {
-	if (['--version', '-v'].includes(optionArray[0])) {
+	if (support.version.commands.includes(optionArray[0])) {
 		process.stdout.write(`Current version: ${packageJson.version}\n`);
 
 		return;
 	}
 
-	if (optionArray[0] === '--repo') {
+	if (optionArray[0] === support.repo.commands) {
 		process.stdout.write(`Visit <${packageJson.homepage}>\n`);
 
 		return;
 	}
 
-	if (optionArray[0] === '--report') {
+	if (optionArray[0] === support.report.commands) {
 		process.stdout.write(
 			`Open an issue at <${packageJson.bugs.url}>,\nor mail to <${packageJson.bugs.email}>\n`,
 		);
